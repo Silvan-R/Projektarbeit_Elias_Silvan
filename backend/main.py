2a738a20e243cf3e4cb3ce9d1d5d7b3a6964d506
Silvan Ryser <silvan.ryser@students.fhnw.ch>
2025-12-10 11:40:05 +0100

    Hallo welt in Backend

--- backend/main.py
+++ backend/main.py
@@ -4,19 +4,16 @@ import pandas as pd
 
 app = FastAPI()
 
-# CORS aktivieren – sonst blockiert der Browser alles!
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # oder ["http://localhost:5173"]
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-df = pd.read_json("Teildatensatz.json")
-
-@app.get("/data")
-def get_data():
-    return df.head(5).to_dict(orient="records")
+@app.get("/")
+def get_hello_world():
+    return {"message": "Hallo Welt test"}
 
 
