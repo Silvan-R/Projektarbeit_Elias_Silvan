c9d664b41ed9f4fee8bc33aa0eae00bc59c24169
Silvan Ryser <silvan.ryser@students.fhnw.ch>
2025-12-10 14:57:03 +0100

    Gesamtdatensatz laden_20251210
    Gesamtdatensatz ins backend laden

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import json
 import pandas as pd
 
 app = FastAPI()
@@ -7,13 +8,20 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+# CSV laden
+df = pd.read_csv("Gesamtdatensatz.csv")
+
 @app.get("/")
-def get_hello_world():
-    return {"message": "Hallo Welt test"}
+def get_backend_lauft():
+    return {"message": "backend lauft!!!"}
+
+
+@app.get("/df")
+def get_dataframe():
+    return {"timestamp": str(df.loc[1, "timestamp"])}
 
 
