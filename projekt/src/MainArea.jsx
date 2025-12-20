716c88f343fd82638ff82d1ee460ffa96d9547d0
elias284 <elias.roos@students.fhnw.ch>
2025-12-20 16:54:23 +0100

    Grafische_Elemente angepasst

--- projekt/src/MainArea.jsx
+++ projekt/src/MainArea.jsx
@@ -10,6 +10,11 @@ export function MainArea({
   if (!kinderanteil || !kinderanteil.Werte) {
     return <main className="main-area">Keine Daten</main>;
   }
+  // für Text der Fokusfrage
+  const istStandardFokusfrage =
+    selectedLocation === "Bahnhofstrasse (Mitte)" &&
+    startDate === "2022-01" &&
+    endDate === "2023-01";
 
   const spec = {
     $schema: "https://vega.github.io/schema/vega-lite/v5.json",
@@ -17,8 +22,16 @@ export function MainArea({
     height: 300,
 
     data: {
-      values: kinderanteil.Werte, // DAS sind deine Daten
+      values: kinderanteil.Werte, // Das sind deine Daten
     },
+    //Kinderanteil nur bis 5% anzeigen
+    transform: [
+      {
+        calculate:
+          "datum.kinderanteil_prozent > 5 ? 5 : datum.kinderanteil_prozent",
+        as: "kinderanteil_abgeschnitten",
+      },
+    ],
 
     mark: "bar",
 
@@ -43,7 +56,7 @@ export function MainArea({
         ],
       },
       y: {
-        field: "kinderanteil_prozent",
+        field: "kinderanteil_abgeschnitten",
         type: "quantitative",
         title: "Kinderanteil in %",
         scale: { domain: [0, 5] },
@@ -54,15 +67,43 @@ export function MainArea({
       ],
     },
   };
-
+  // Für Abfrage höchster %-Wert
+  const maxEintrag = kinderanteil.Werte.reduce((max, curr) => {
+    if (
+      typeof curr.kinderanteil_prozent !== "number" ||
+      isNaN(curr.kinderanteil_prozent)
+    ) {
+      return max;
+    }
+    return curr.kinderanteil_prozent > max.kinderanteil_prozent ? curr : max;
+  }, kinderanteil.Werte[0]);
   return (
     <main className="main-area">
-      <h3>
-        Kinderanteil am Standort {selectedLocation} zwischen {startDate} und{" "}
-        {endDate}
-      </h3>
+      {istStandardFokusfrage ? (
+        <h3>
+          Fokusfrage: In welchem Monat im Jahr 2022 ist der Anteil der Kinder im
+          Vergleich zu den Erwachsenen Fussgängern am grössten am Ort
+          Bahnhofstrasse Mitte?
+        </h3>
+      ) : (
+        <h3>
+          Kinderanteil am Standort {selectedLocation} zwischen {startDate} und{" "}
+          {endDate}
+        </h3>
+      )}
 
       <VegaLite spec={spec} />
+      <p className="chart-info">
+        Hinweis: Der Datensatz ist teilweise lückenhaft. In einzelnen Monaten
+        wurden unrealistisch hohe Kinderanteile erfasst. Um eine vergleichbare
+        Darstellung zu ermöglichen, wurden Kinderanteile in der Visualisierung
+        bei maximal 5% begrenzt.
+      </p>
+
+      <h3>
+        Höchster Kinderanteil im ausgewählten Zeitraum: {maxEintrag.month}(
+        {maxEintrag.kinderanteil_prozent} %){" "}
+      </h3>
     </main>
   );
 }
